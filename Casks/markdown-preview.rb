0ac690038885e2ea5493eae873c069f0aa2ed0b5
Fauzaan <mfauzaan@icloud.com>
2026-05-07 14:04:27 +0000

    markdown-preview 0.0.17,21

--- Casks/markdown-preview.rb
+++ Casks/markdown-preview.rb
@@ -1,6 +1,6 @@
 cask "markdown-preview" do
-  version "0.0.16,20"
-  sha256 "585cc258fd82012bb54de5f4b6e6f2366c3b945fb593d629519047f9c80cf899"
+  version "0.0.17,21"
+  sha256 "a7a6bf30fccfb4d17447abc37f464baa9d025360e48ad764e525bd3b8e38a01a"
 
   url "https://cdn.amore.computer/releases/doc.md-preview/#{version.csv.first}-#{version.csv.second}/Markdown-Preview.dmg",
       verified: "cdn.amore.computer/releases/doc.md-preview/"
